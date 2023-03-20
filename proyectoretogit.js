2f731cebad52c5d729991f21bb9c55f976e9306f
Marcelcoder001 <marcel.soto324@gmail.com>
2023-03-20 12:37:52 +0000

    second draft

---
+++ proyectoretogit.js
@@ -0,0 +1,14 @@
+function suma (x, y){
+    console.log(x + y)} 
+
+function resta (x, y){
+    console.log(x - y)} 
+        
+function producto (x, y){
+    console.log(x * y)} 
+
+function division (x, y){
+        console.log(x / y)} 
+
+function cuadrado(x){
+    console.log(x * x)}
